a774f845ae11ad2fdcef207c862041a747e8ef3d
jonas <jonas.kazlauskas@me.com>
2021-10-28 08:18:46 -0400

    Add platformio framework

---
+++ Controls/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  Serial.println("test");
+  // put your main code here, to run repeatedly:
+}
